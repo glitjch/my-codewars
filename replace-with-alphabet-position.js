adb439f56b468b7a996ee42e73e3fe482c27272f
TJ Phan <t.jamesphan@gmail.com>
2022-04-20 19:40:48 -0700

    refactored

--- replace-with-alphabet-position.js
+++ replace-with-alphabet-position.js
@@ -11,11 +11,13 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 */
 
 function alphabetPosition(text) {
-  let lowerCase = text.toLowerCase();
-  let onlyLetters = lowerCase.split("").filter(char => char.match(/[a-z]/i));
-  let convertToletterNums = onlyLetters.map(letter => letter.charCodeAt(letter[0]) -96).join(" ");
-  return convertToletterNums;
-}
-
+  if (text !== null) {
+  const regex = /[a-z]/gi;
+  let onlyLetters = text.toLowerCase().match(regex);
+  let convertToLetterNums = onlyLetters
+    .map((char) => char.charCodeAt() - 96)
+    .join(" ");
+  return convertToLetterNums;
+  }}
 alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11";
 alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20";
