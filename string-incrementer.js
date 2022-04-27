454b9e69ed21debadf81429b742409e340cfda5f
TJ Phan <t.jamesphan@gmail.com>
2022-04-27 12:40:36 -0700

    start challenge

---
+++ string-incrementer.js
@@ -0,0 +1,34 @@
+/**
+ * If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+ */
+
+function incrementString (strng) {
+  // return incrementedString
+}
+
+/**
+ * describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(incrementString("foobar000"), "foobar001");
+Test.assertEquals(incrementString("foo"), "foo1");
+Test.assertEquals(incrementString("foobar001"), "foobar002");
+Test.assertEquals(incrementString("foobar99"), "foobar100");
+Test.assertEquals(incrementString("foobar099"), "foobar100");
+Test.assertEquals(incrementString(""), "1");
+  });
+});
+
+ */
