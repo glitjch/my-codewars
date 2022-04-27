4ecdb8db011810d131425fb36b16e1455267750a
TJ Phan <t.jamesphan@gmail.com>
2022-04-27 14:14:58 -0700

    complete challenge

--- string-incrementer.js
+++ string-incrementer.js
@@ -15,10 +15,37 @@ foo099 -> foo100
 Attention: If the number has leading zeros the amount of digits should be considered.
  */
 
-function incrementString (strng) {
-  // return incrementedString
+// function incrementString (string) {
+//   let lastChar = string[string.length -1];
+//   let test = Number(lastChar) 
+//   if (isNaN(test)) {
+//     return `${string}1`;
+//   } 
+//   let replaceLastWith = +lastChar + 1
+//   let replacedString = string.replace(lastChar, replaceLastWith);
+//   let differenceInLength = replacedString.length > string.length;
+//   if (differenceInLength) {
+
+//   }
+// }
+
+// TODO
+
+function incrementString (string) {
+  let lastChar = string[string.length -1];
+  let test = Number(lastChar) 
+  if (isNaN(test)) {
+    return `${string}1`;
+  } 
+  let getNums = string.split("").filter(char => !isNaN(Number(char))).join("")
+  let numOfDigits = getNums;
+  console.log(getNums);
+  let replaceLastWith = +lastChar + 1
+
+  return string.replace(lastChar, replaceLastWith);
 }
 
+console.log(incrementString("foobar009"))
 /**
  * describe("Tests", () => {
   it("test", () => {
