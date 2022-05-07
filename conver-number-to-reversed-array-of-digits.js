3a1af3aa74fb11b8bc57fc4d96eba1a4dac83485
TJ Phan <t.jamesphan@gmail.com>
2022-05-07 00:21:27 -0700

    Add challenge

---
+++ conver-number-to-reversed-array-of-digits.js
@@ -0,0 +1,29 @@
+/**
+ * Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0]
+
+ */
+function digitize(n) {
+  const array =  n.toString().split("");
+  [...rest] = array.reverse().map(str => Number(str));
+  return rest
+};
+
+console.log(digitize(35231))
+/**
+ * const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(digitize(35231),[1,3,2,5,3]);
+    assert.deepEqual(digitize(0),[0]);
+  });
+});
+
+ */
