64d97d795db848da7522441b9a23a842944cf103
TJ Phan <t.jamesphan@gmail.com>
2022-05-10 13:00:24 -0700

    Add challenge

---
+++ exes-and-ohs.js
@@ -0,0 +1,31 @@
+/**
+ * Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+ */
+
+function XO(str) {
+  const array = str.toLowerCase().split("");
+  const exes = array.filter(e => e === "x");
+  const ohs = array.filter(e => e === "o");
+  return exes.length === ohs.length ? true : false;
+};
+
+/**
+ * describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(XO('xo'),true);
+Test.assertEquals(XO("xxOo"),true);
+Test.assertEquals(XO("xxxm"),false);
+Test.assertEquals(XO("Oo"),false);
+Test.assertEquals(XO("ooom"),false);
+  });
+});
+
+ */
