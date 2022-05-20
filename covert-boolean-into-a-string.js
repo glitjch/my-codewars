2a88e55898cc0ed5b59eebe4d5d53821a0fd697f
TJ Phan <t.jamesphan@gmail.com>
2022-05-20 11:49:50 -0700

    Add challenge

---
+++ covert-boolean-into-a-string.js
@@ -0,0 +1,20 @@
+/**
+ * mplement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+ */
+
+function booleanToString(b){
+ return b ? "true" : "false";  
+};
+
+/**
+ * describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+  });
+});
+
+ */
