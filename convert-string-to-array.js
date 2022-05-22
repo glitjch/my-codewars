6a138c56fd36d4282cac55643f8205ba98c89bb8
TJ Phan <t.jamesphan@gmail.com>
2022-05-22 11:11:00 -0700

    Add challenge

---
+++ convert-string-to-array.js
@@ -0,0 +1,27 @@
+/**
+ * Write a function to split a string and convert it into an array of words.
+
+Examples (Input -> Output):
+* "Robin Singh" ==> ["Robin", "Singh"]
+
+* "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+FUNDAMENTALSARRAYSSTRINGS
+ */
+
+function stringToArray(string){
+  return string.split(" ");
+
+}
+
+/**
+ * const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+    assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
+  });
+});
+ */
