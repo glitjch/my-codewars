1e2cd6fccd90a0a43eec1057e99e0ac44f874ebb
TJ Phan <t.jamesphan@gmail.com>
2022-06-12 13:01:30 -0800

    Add challeng

---
+++ jaden-casing-strings.js
@@ -0,0 +1,45 @@
+/**
+ * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Link to Jaden's former Twitter account @officialjaden via archive.org
+
+
+ */
+// convert to an array and iterate looking for a space
+// convert the letter after into uppercase.
+String.prototype.toJadenCase = function () {
+    
+  let newString = "";
+  
+  for (let letter = 0; letter < this.length; letter++){
+    if (letter === 0) {
+      newString += this[letter].toUpperCase();
+    }
+    else if (this[letter] === " ") {
+      newString += " " + this[letter + 1].toUpperCase();
+      letter++;
+    } else {
+      newString += this[letter];
+    }
+  }
+  return newString;
+};
+
+console.log(toJadenCase("How can mirrors be real if our eyes aren't real"))
+
+/**
+ * describe("Tests", () => {
+  it("test", () => {
+var str = "How can mirrors be real if our eyes aren't real";
+Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+
+  });
+});
+
+ */
